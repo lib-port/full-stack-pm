95b83932da237add4cf1093630fec82d0c0ba8bb
lib-port <lib-port@users.noreply.github.com>
2026-09-22 04:27:10 -0400

    Updated favicon: light/dark responsive

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,6 @@ const repositoryUrl = 'https://github.com/lib-port/full-stack-pm';
 const config = {
   title: 'Full-Stack Product Manager',
   tagline: 'Decide, Design, and Deliver with AI',
-  favicon: 'img/logo.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
